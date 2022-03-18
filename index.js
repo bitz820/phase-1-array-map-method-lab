c22c3d74a1e336ef0b0232cfd143d178e70b20fa
Mark <mark.n.bacon.jr@gmail.com>
2022-03-18 00:24:59 -0400

    commit tree babcd45587c0be1e29c7b032f62ecca2b1c71733

--- index.js
+++ index.js
@@ -11,6 +11,57 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+// 1. get access to each sentence 
+// 2. isolate each eachWord
+// 3. capitalize each first letter only
+// 4. return the array
+
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((sentence) =>{
+    const eachWord = sentence.split(' ')
+    const capFirst = eachWord.map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    })
+    const answer = capFirst.join(' ')
+    return answer
+  })
 }
+titleCased()
+
+// const titleCased = () => {
+//   let words = tutorials.split(' ');
+//   let firstLetter = tutorials.map(words => words.charAt(0).toUpperCase());
+//   let restOfWord = tutorials.map((words) => words.slice(1));
+//   return firstLetter + restOfWord
+// }
+
+// function titleCaseString(arr){
+//   let eachWord = arr.split(' ');
+//   // if (eachWord[0]!==Number){
+//   //   return eachWord[0].toUpperCase()
+//   // }
+//   console.log(eachWord)
+// }
+
+
+// function callback(array) {
+//   for (let index of array) {
+//     let singleWord = index.split(' ');
+//     console.log(singleWord)
+//     for (let i = 0; i < singleWord.length; i++) {
+  
+//       if (singleWord[i][0] !== Number) {
+//         singleWord[i] = singleWord[i][0].toUpperCase() + singleWord[i].slice(1);
+//       }
+//       singleWord.join('')
+//     }
+//     return singleWord
+//   }
+// }
+// callback(tutorials)
+
+// const titleCased = tutorials.map(() => {
+//   const makeLetterUpper = 
+// })
